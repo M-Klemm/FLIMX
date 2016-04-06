67e3e15d5b9ecd83fa33906bdea46e0b53d08776
M-Klemm <matthias.klemm@tu-ilmenau.de>
2016-04-06 15:11:00 +0200

    Change: updated required Matlab version to R2014b

--- FLIMXLauncher.m
+++ FLIMXLauncher.m
@@ -3,8 +3,8 @@
 
 %check Matlab version
 nr = version('-release');
-if(str2double(nr(1:4)) < 2013)
-    uiwait(errordlg(sprintf('Your MATLAB version is R%s. This software requires MATLAB R2011a or newer. Please update your MATLAB installation.',nr),'MATLAB version too old','modal'));
+if(~(str2double(nr(1:4)) > 2014 || str2double(nr(1:4)) == 2014 && strcmp(nr(end),'b')))
+    uiwait(errordlg(sprintf('Your MATLAB version is R%s. This software requires MATLAB R2014b or newer. Please update your MATLAB installation.',nr),'MATLAB version too old','modal'));
     return
 end
 clear nr;
