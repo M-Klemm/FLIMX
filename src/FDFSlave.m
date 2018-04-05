c54ab0c12538f20cd67104c1b2a6b1981dc08df9
M-Klemm <matthias_klemm@gmx.net>
2018-04-05 19:03:34 +0200

    Change: renamed 'functions' folder to 'src'

---
+++ src/FDFSlave.m
@@ -0,0 +1,14 @@
+function FDFSlave(varargin)
+if(~isdeployed())
+    addpath(genpath(fullfile(cd,'src')));
+end
+if(nargin == 0)
+    path = cd;
+else
+    path = varargin{1,1};
+end
+warning('off','MATLAB:rankDeficientMatrix');
+disp(path);
+pause(rand(1)/2+0.25);
+startmulticoreslave(path);
+
