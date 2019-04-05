2c17bf21c265d9820ef88fe3407478ecf5f266e6
M-Klemm <matthias_klemm@gmx.net>
2019-04-05 15:04:02 +0200

    Fix: sign error introduced in layer approach fix some time ago causing spatial artifacts in fluorescence lifetimes

--- src/FLIMX/@FLIMX/FLIMX.m
+++ src/FLIMX/@FLIMX/FLIMX.m
@@ -478,8 +478,8 @@ classdef FLIMX < handle
             out.config_revision = 266;
             out.client_revision_major = 4;
             out.client_revision_minor = 5;
-            out.client_revision_fix = 14;
-            out.core_revision = 372;
+            out.client_revision_fix = 15;
+            out.core_revision = 373;
             out.results_revision = 256;
             out.measurement_revision = 205;
         end
