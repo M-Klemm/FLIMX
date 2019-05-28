f0cdb1a3a51af718bb71db171ec1e5951371a5f1
M-Klemm <matthias_klemm@gmx.net>
2019-05-28 16:31:56 +0200

    Fix: changing the ROA in FLIMFit was not saved correctly

--- src/FLIMX/@FLIMX/FLIMX.m
+++ src/FLIMX/@FLIMX/FLIMX.m
@@ -484,7 +484,7 @@ classdef FLIMX < handle
             out.config_revision = 267;
             out.client_revision_major = 4;
             out.client_revision_minor = 9;
-            out.client_revision_fix = 0;
+            out.client_revision_fix = 1;
             out.core_revision = 369;
             out.results_revision = 256;
             out.measurement_revision = 205;
--- src/FLIMX/fileAccess/measurements/@measurementFile/measurementFile.m
+++ src/FLIMX/fileAccess/measurements/@measurementFile/measurementFile.m
@@ -1008,7 +1008,9 @@ classdef measurementFile < handle
                 this.exportMatFile(ch,'');
                 %this.filesOnHDD(1,ch) = true;                
             end
-            this.checkMyFiles();
+            if(length(this.filesOnHDD) < ch || ~this.filesOnHDD(ch))
+                this.checkMyFiles();
+            end
         end
         
         function exportMatFile(this,ch,folder)
