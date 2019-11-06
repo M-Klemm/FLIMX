25a276ca62727897520cf727308ae54cd44928da
M-Klemm <matthias_klemm@gmx.net>
2019-11-06 15:12:25 +0100

    Fix: commandline output was empty when approximating all channels of a subject

--- src/FLIMX/@FLIMX/FLIMX.m
+++ src/FLIMX/@FLIMX/FLIMX.m
@@ -490,7 +490,7 @@ classdef FLIMX < handle
             out.config_revision = 267;
             out.client_revision_major = 4;
             out.client_revision_minor = 10;
-            out.client_revision_fix = 0;
+            out.client_revision_fix = 1;
             out.core_revision = 410;
             out.results_revision = 256;
             out.measurement_revision = 205;
