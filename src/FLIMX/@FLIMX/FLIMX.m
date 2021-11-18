0656b38db0b80869e6a7823a5ccd1338e7756eb7
M-Klemm <Matthias_Klemm@gmx.net>
2021-11-18 19:14:30 +0100

    Fix: ROI coordinates of an condition MV group (scatter plot) can now be applied to the underlying MV groups of each subject

--- src/FLIMX/@FLIMX/FLIMX.m
+++ src/FLIMX/@FLIMX/FLIMX.m
@@ -555,7 +555,7 @@ classdef FLIMX < handle
             out.config_revision = 276;
             out.client_revision_major = 5;
             out.client_revision_minor = 7;
-            out.client_revision_fix = 0;
+            out.client_revision_fix = 1;
             out.core_revision = 503;
             out.results_revision = 256;
             out.measurement_revision = 206;
