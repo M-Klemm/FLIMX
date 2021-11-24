9ff57299629c56be9b360d7544fbb57ffa919c49
M-Klemm <matthias_klemm@gmx.net>
2021-11-24 16:17:55 +0100

    Fix: workaround for error caused by Matlab's bar function when an input is empty

--- src/FLIMX/@FLIMX/FLIMX.m
+++ src/FLIMX/@FLIMX/FLIMX.m
@@ -555,7 +555,7 @@ classdef FLIMX < handle
             out.config_revision = 276;
             out.client_revision_major = 5;
             out.client_revision_minor = 7;
-            out.client_revision_fix = 2;
+            out.client_revision_fix = 3;
             out.core_revision = 503;
             out.results_revision = 256;
             out.measurement_revision = 206;
